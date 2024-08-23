18239fc771c59873fbfb0706d07f26b0b1b8f0e6
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2024-08-23 11:16:22 +0100

    added book function base views

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'book-urls'
 urlpatterns = [
-    path('', views.list_authors, name='list-author'),
+    path('author/', views.list_authors, name='list-author'),
+    path('tag/', views.list_tags, name='list-tag'),
+    path('list/', views.list_books, name='list-book'),
+    path('create/', views.create_book, name='create-book')
 ]
